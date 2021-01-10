1460cfc24afb6b36c470c81a12dd0961afd75382
nguyentrongty <62439357+nguyentrongty@users.noreply.github.com>
2021-01-10 21:31:19 +0700

    Eshop1

---
+++ Eshop1/MovieApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieApp.Models;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAppRepositiry repositiry;
+        public int PageSize = 4;
+
+        public HomeController(IAppRepositiry repo)
+        {
+            repositiry = repo;
+        }
+
+        //public IActionResult Index() => View(repositiry.Products);
+
+        public ViewResult Index(int productPage = 1)
+            => View(repositiry.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize)
+                );
+    }
+}
---
+++ Eshop1/MovieApp/Models/EFAppRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models
+{
+    public class EFAppRepository : IAppRepositiry
+    {
+        private AppDbContext context;
+
+        public EFAppRepository(AppDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
